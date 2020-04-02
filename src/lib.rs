1819cc015211f8cdd5340ed9c9c204bd5e4c5f50
shane <moloney.shane@yahoo.com>
2020-04-02 21:32:14 +0100

    added some hyper deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
+extern crate hyper;
+
 mod utils;
 
+use hyper::{Client, Uri};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -13,7 +16,29 @@ extern {
     fn alert(s: &str);
 }
 
+
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, hism!");
+pub fn request_ip()-> String {
+    let client = Client::new();
+
+    let future = client.get(Uri::from_static("http://httpbin.org/ip"));
+    future.and_then().collect().to_string()
 }
+
+// was attempting to use hyper to do  Http Requests but i have no idea what im doing yet. Also, I need to have some sort of homepage up and running first!
+
+//type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+
+/*#[tokio::main]
+async fn main() -> Result<()> {
+// http requests
+  //  let client = Client::new();
+    let uri = "http://httpbin.org/ip".parse()?;
+    let resp = client.get(uri).await?;
+
+Ok(())
+}*/
+
+
+
+
